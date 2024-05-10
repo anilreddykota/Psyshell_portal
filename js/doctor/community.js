5a4c8202cae3feb3595d0dcd28df364528411031
anilreddykota <22b91a5469@srkrec.ac.in>
2024-05-10 20:48:56 +0530

    bug displaying posts

--- js/doctor/community.js
+++ js/doctor/community.js
@@ -1,38 +1,49 @@
-
 const puid = localStorage.puid;
 
-
-fetch('https://atman.onrender.com/get-newsfeed')
-  .then(response => response.json())
-  .then(data => {
+fetch("https://atman.onrender.com/get-newsfeed")
+  .then((response) => response.json())
+  .then((data) => {
     // Process the received JSON data
-    document.getElementById('posts-container').innerHTML = "";
+    document.getElementById("posts-container").innerHTML = "";
     if (data.posts && data.posts.length > 0) {
       // Iterate over posts data
-      data.posts.forEach(post => {
+      data.posts.forEach((post) => {
         // Generate HTML markup for each post
-        const date = new Date(post.date._seconds * 1000 + Math.round(post.date._nanoseconds / 1000000));
+        const date = new Date(
+          post.date._seconds * 1000 +
+            Math.round(post.date._nanoseconds / 1000000)
+        );
         // Format the date and time
 
-        const formattedDateTime = date.toLocaleString('en-US', {
-          year: 'numeric',
-          month: '2-digit',
-          day: '2-digit',
-          hour: '2-digit',
-          minute: '2-digit',
-          second: '2-digit'
+        const formattedDateTime = date.toLocaleString("en-US", {
+          year: "numeric",
+          month: "2-digit",
+          day: "2-digit",
+          hour: "2-digit",
+          minute: "2-digit",
+          second: "2-digit",
         });
         const postHTML = `<div class="central-meta item">
         <div class="user-post">
             <div class="friend-info">
                 <figure>
-                    <img src="${post?.userDetails?.profile || "./images/resources/defaultpic.jpg"}" alt="" class='card__avatar profile-round'>
+                    <img src="${
+                      post?.userDetails?.profile ||
+                      "./images/resources/defaultpic.jpg"
+                    }" alt="" class='card__avatar profile-round'>
                 </figure>
                 <div class="friend-name">
-                <ins><a  title="">${post.userDetails?.nickname || "unknown user"}</a>  
-                ${post.userDetails?.badges ? post?.userDetails?.badges?.map((badge) =>
-          ` <img src="./images/badges/${badge}.png" class="user-badges">`
-        ) : ''}
+                <ins><a  title="">${
+                  post.userDetails?.nickname || "unknown user"
+                }</a>  
+                ${
+                  post?.userDetails?.badges
+                    ? post?.userDetails?.badges?.map(
+                        (badge) =>
+                          ` <img src="./images/badges/${badge}.png" class="user-badges">`
+                      )
+                    : ""
+                }
                </ins>
                     <span>published: ${formattedDateTime}</span>
                 </div>
@@ -63,7 +74,12 @@ fetch('https://atman.onrender.com/get-newsfeed')
                     <li>
                         <span class="like" data-toggle="tooltip" title="like">
                             <button class="like-button">
-                                <div class="like-wrapper like-btn ${post?.likedBy?.likes[puid] == true ? 'liked' : ''}"  key="${post.postId}" isliked="${!!post?.likedBy?.likes[puid]}">
+                                <div class="like-wrapper like-btn ${
+                                  post?.likedBy?.likes[puid] == true
+                                    ? "liked"
+                                    : ""
+                                }"  key="${post.postId}" isliked="${!!post
+          ?.likedBy?.likes[puid]}">
                                     <svg class="heart" width="24" height="24" viewBox="0 0 24 24">
                                         <path d="M12,21.35L10.55,20.03C5.4,15.36 2,12.27 2,8.5C2,5.41 4.42,3 7.5,3C9.24,3 10.91,3.81 12,5.08C13.09,3.81 14.76,3 16.5,3C19.58,3 22,5.41 22,8.5C22,12.27 18.6,15.36 13.45,20.03L12,21.35Z"></path>
                                     </svg>
@@ -82,7 +98,9 @@ fetch('https://atman.onrender.com/get-newsfeed')
 
                     </li>
                     <li>
-                    <p><span class="ins like-button" key="${post.postId}" >${post.likesCount}</span></p>
+                    <p><span class="ins like-button" key="${post?.postId}" >${
+          post?.likesCount
+        }</span></p>
                     </li>
                 </ul>
                     </div>
@@ -93,26 +111,51 @@ fetch('https://atman.onrender.com/get-newsfeed')
        
         <div class="form-group">
             <card-with-comments class="card-with-comments" role="article">
-            ${post.comments.length > 0 ? `
+            ${
+              post?.comments?.length > 0
+                ? `
                 <div class="comments">
-                    ${post.comments.map(comment => `
+                    ${post?.comments
+                      ?.map(
+                        (comment) => `
                         <div class="comment">
                             <div class="comment__header">
-                                <img src="${comment.commenterDetails.profile || "./images/resources/defaultpic.jpg"}" alt="Comment writer image" class="comment__avatar profile-round">
-                                <p class="comment__user-name">${comment.commenterDetails.nickname || "unknown user"}</p>
-                                <p class="comment__time text-success">${formatTimeDifferences(comment.timestamp)}</p>
-                                <p class="comment__chip">${comment.commenterDetails.role || "Psycholigist"}</p>
+                                <img src="${
+                                  comment.commenterDetails.profile ||
+                                  "./images/resources/defaultpic.jpg"
+                                }" alt="Comment writer image" class="comment__avatar profile-round">
+                                <p class="comment__user-name">${
+                                  comment.commenterDetails.nickname ||
+                                  "unknown user"
+                                }</p>
+                                <p class="comment__time text-success">${formatTimeDifferences(
+                                  comment.timestamp
+                                )}</p>
+                                <p class="comment__chip">${
+                                  comment.commenterDetails.role ||
+                                  "Psycholigist"
+                                }</p>
                             </div>
                             <p class="comment__text">${comment.comment}</p>
                         </div>
-                    `).join('')
-                  }
+                    `
+                      )
+                      .join("")}
                 </div>
-                ` : ''}
+                `
+                : ""
+            }
                 <div class="form">
-                <img src="${`https://firebasestorage.googleapis.com/v0/b/psycove-4ebf5.appspot.com/o/profilepics%2F${localStorage.puid}?alt=media` || "../images/resources/defaultpic.jpg"}" alt="Your avatar" class="comment__avatar">
-                <input type="text" class="form__input" placeholder="Share your thoughts on this post" id="commentText_${post.postId}">
-                <button class="form__button" onclick="addcomment('${post.postId}','${localStorage.puid}')"></button>
+                <img src="${
+                  `https://firebasestorage.googleapis.com/v0/b/psycove-4ebf5.appspot.com/o/profilepics%2F${localStorage.puid}?alt=media` ||
+                  "../images/resources/defaultpic.jpg"
+                }" alt="Your avatar" class="comment__avatar">
+                <input type="text" class="form__input" placeholder="Share your thoughts on this post" id="commentText_${
+                  post.postId
+                }">
+                <button class="form__button" onclick="addcomment('${
+                  post.postId
+                }','${localStorage.puid}')"></button>
             </div>
             
             </card-with-comments>
@@ -120,114 +163,105 @@ fetch('https://atman.onrender.com/get-newsfeed')
         
     </div>`;
 
-        document.getElementById('posts-container').innerHTML += postHTML;
-        const likeButtons = document.querySelectorAll('.like-btn');
+        document.getElementById("posts-container").innerHTML += postHTML;
+        const likeButtons = document.querySelectorAll(".like-btn");
         if (puid) {
-
-
-          likeButtons?.forEach(like => {
-            like.addEventListener('click', async (event) => {
+          likeButtons?.forEach((like) => {
+            like.addEventListener("click", async (event) => {
               try {
-                const uniqueKey = like.getAttribute('key');
-                const isLiked = like.getAttribute('isliked');
+                const uniqueKey = like.getAttribute("key");
+                const isLiked = like.getAttribute("isliked");
                 const ins = document.querySelector(`.ins[key="${uniqueKey}"]`);
-                const puid = localStorage.getItem('puid');
+                const puid = localStorage.getItem("puid");
                 const queryParams = new URLSearchParams();
-                queryParams.append('postid', uniqueKey);
-                queryParams.append('uid', puid);
+                queryParams.append("postid", uniqueKey);
+                queryParams.append("uid", puid);
                 let url;
 
-
-                if (isLiked == 'true') {
+                if (isLiked == "true") {
                   url = `https://atman.onrender.com/dislike-post?${queryParams.toString()}`;
                   ins.textContent = Math.max(parseInt(ins.textContent) - 1, 0);
-             
-                  like.setAttribute('isliked', 'false');
-                  showToast("like removed from post");
 
+                  like.setAttribute("isliked", "false");
+                  showToast("like removed from post");
                 } else {
                   url = `https://atman.onrender.com/like-post?${queryParams.toString()}`;
                   ins.textContent = parseInt(ins.textContent) + 1;
-  
+
                   showToast(" your like added to post");
 
-                  like.setAttribute('isliked', 'true');
+                  like.setAttribute("isliked", "true");
                 }
 
-
                 const response = await fetch(url, {
-                  method: 'POST'
+                  method: "POST",
                 });
                 const result = await response.json();
-                if (result.message === 'Post liked successfully' ) {
-                  
-                  like.setAttribute('isliked', 'true');
+                if (result.message === "Post liked successfully") {
+                  like.setAttribute("isliked", "true");
                   // Optionally, update UI to reflect the change in like status
-                }else if(result.message === 'Post disliked successfully'){
-                  like.setAttribute('isliked', 'false');
+                } else if (result.message === "Post disliked successfully") {
+                  like.setAttribute("isliked", "false");
                 } else {
-                  console.error('Failed to send like request:', response.statusText);
+                  console.error(
+                    "Failed to send like request:",
+                    response.statusText
+                  );
                   // Optionally, display an error message to the user
                 }
               } catch (error) {
-                console.error('Error sending like request:', error);
+                console.error("Error sending like request:", error);
                 // Handle network or other errors
               }
             });
-
           });
         }
       });
-
     } else {
-      console.log('No posts found');
+      console.log("No posts found");
     }
   })
-  .catch(error => {
-    console.error('Error fetching posts:', error);
+  .catch((error) => {
+    console.error("Error fetching posts:", error);
   });
 
+document.addEventListener("DOMContentLoaded", function () {
+  const createPostForm = document.getElementById("createPostForm");
+  const imagePreview = document.getElementById("imagePreview");
+  var imageref = document.getElementById("image");
 
-
-
-document.addEventListener('DOMContentLoaded', function () {
-  const createPostForm = document.getElementById('createPostForm');
-  const imagePreview = document.getElementById('imagePreview');
-  var imageref = document.getElementById('image');
-
-  imageref?.addEventListener('change', (event) => {
+  imageref?.addEventListener("change", (event) => {
     imagePreview.src = URL.createObjectURL(event.target.files[0]);
-    imagePreview.style.display = 'block';
+    imagePreview.style.display = "block";
     imagePreview.onload = function () {
-      URL.revokeObjectURL(imagePreview.src)
-    }
-
-  })
-  createPostForm?.addEventListener('submit', async function (event) {
+      URL.revokeObjectURL(imagePreview.src);
+    };
+  });
+  createPostForm?.addEventListener("submit", async function (event) {
     event.preventDefault();
 
     // Validate form fields
-    const title = document.getElementById('title').value.trim();
-    const description = document.getElementById('description').value.trim();
-    const image1 = document.getElementById('image')?.files[0];
+    const title = document.getElementById("title").value.trim();
+    const description = document.getElementById("description").value.trim();
+    const image1 = document.getElementById("image")?.files[0];
 
     const puid = localStorage.puid;
     if (!title || !description || !image1) {
-      alert('Please fill in all fields');
+      alert("Please fill in all fields");
       return;
     }
 
     // Set puid value
 
-    document.getElementById('uid').value = puid;
+    document.getElementById("uid").value = puid;
 
     // Create FormData object
     const formData = new FormData(this);
 
     try {
-      const response = await fetch('https://atman.onrender.com/create-post', {
-        method: 'POST',
-        body: formData
+      const response = await fetch("https://atman.onrender.com/create-post", {
+        method: "POST",
+        body: formData,
       });
 
       if (response.ok) {
@@ -236,20 +270,21 @@ document.addEventListener('DOMContentLoaded', function () {
         window.location.reload();
         // Optionally, redirect to another page or display a success message
       } else {
-        console.error('Failed to create post:', response.statusText);
+        console.error("Failed to create post:", response.statusText);
         // Handle error appropriately
       }
     } catch (error) {
-      console.error('Error creating post:', error);
+      console.error("Error creating post:", error);
       // Handle error appropriately
     }
   });
 });
 
-
 function formatTimeDifferences(timestamp) {
   const currentTime = new Date();
-  const commentTime = new Date(timestamp._seconds * 1000 + Math.round(timestamp._nanoseconds / 1000000));
+  const commentTime = new Date(
+    timestamp._seconds * 1000 + Math.round(timestamp._nanoseconds / 1000000)
+  );
 
   const differenceInSeconds = Math.floor((currentTime - commentTime) / 1000);
   const differenceInMinutes = Math.floor(differenceInSeconds / 60);
@@ -257,17 +292,23 @@ function formatTimeDifferences(timestamp) {
   const differenceInDays = Math.floor(differenceInHours / 24);
   if (differenceInDays > 7) {
     // If it crosses more than a week, display the date
-    const options = { year: 'numeric', month: 'short', day: 'numeric' };
-    return commentTime.toLocaleDateString('en-US', options);
+    const options = { year: "numeric", month: "short", day: "numeric" };
+    return commentTime.toLocaleDateString("en-US", options);
   } else if (differenceInDays > 0) {
     // If it's within a week, but more than a day, display days ago
-    return differenceInDays === 1 ? '1 day ago' : `${differenceInDays} days ago`;
+    return differenceInDays === 1
+      ? "1 day ago"
+      : `${differenceInDays} days ago`;
   } else if (differenceInHours > 0) {
     // If it's within a day, but more than an hour, display hours ago
-    return differenceInHours === 1 ? '1 hr ago' : `${differenceInHours} hrs ago`;
+    return differenceInHours === 1
+      ? "1 hr ago"
+      : `${differenceInHours} hrs ago`;
   } else {
     // If it's within an hour, display minutes ago
-    return differenceInMinutes <= 1 ? 'just now' : `${differenceInMinutes} mins ago`;
+    return differenceInMinutes <= 1
+      ? "just now"
+      : `${differenceInMinutes} mins ago`;
   }
 }
 
@@ -275,28 +316,26 @@ async function addcomment(postid, puid) {
   const commentTextElement = document.getElementById(`commentText_${postid}`);
   const commentText = commentTextElement.value.trim();
 
-
   if (commentText === "") {
     showToast("Please enter a comment.");
     return;
   }
-  const response = await axios.post('https://atman.onrender.com/doctor/addcomment', { comment: commentText, puid: puid, postid });
+  const response = await axios.post(
+    "https://atman.onrender.com/doctor/addcomment",
+    { comment: commentText, puid: puid, postid }
+  );
 
   if (response.data.message === "Comment added successfully.") {
-    showToast(response.data.message)
-    commentTextElement.value = ""
-
+    showToast(response.data.message);
+    commentTextElement.value = "";
   }
-
-
 }
-
 
 // Function to show the toast message
 function showToast(message) {
-  const messageToast = document.getElementById('messageToast');
+  const messageToast = document.getElementById("messageToast");
   messageToast.innerText = message;
-  messageToast.style.display = 'block'; // Show the message
+  messageToast.style.display = "block"; // Show the message
   setTimeout(() => {
     closeToast(); // Automatically close after 4 seconds
   }, 4000);
@@ -304,14 +343,13 @@ function showToast(message) {
 
 // Function to close the toast message
 function closeToast() {
-  const messageToast = document.getElementById('messageToast');
-  messageToast.style.animation = 'slideOutRight 1s forwards'; // Animation for exit
+  const messageToast = document.getElementById("messageToast");
+  messageToast.style.animation = "slideOutRight 1s forwards"; // Animation for exit
   setTimeout(() => {
-    messageToast.style.display = 'none'; // Hide the message after animation
-    messageToast.style.animation = ''; // Reset animation
+    messageToast.style.display = "none"; // Hide the message after animation
+    messageToast.style.animation = ""; // Reset animation
   }, 1000); // Wait for animation to complete
 }
-
 
 async function deletePost(id) {
   const response = await fetch("https://atman.onrender.com/deletePost", {
